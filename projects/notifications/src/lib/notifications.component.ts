826627a4afe6fefe4e43cea54286454acc224e99
Kelvince01 <themikrochip1@gmail.com>
2023-11-27 21:14:50 +0300

    feat: initial commit

---
+++ projects/notifications/src/lib/notifications.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-notifications',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      notifications works!
+    </p>
+  `,
+  styles: ``
+})
+export class NotificationsComponent {
+
+}
---
+++ projects/notifications/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of notifications
+ */
+
+export * from './lib/notifications.service';
+export * from './lib/notifications.component';
